3e92af74194eaf346739fda664a60217184a504f
Grace <20106694@mail.wit.ie>
2025-09-19 15:10:56 +0100

    Add task functionality

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid';
 import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
@@ -11,16 +12,58 @@ function App() {
       { id: 3, title: "Tidy up", deadline: "Today", priority: "High", done: false }
     ]
   });
+  const [ formState, setFormState ] = useState({
+    title: "",
+    description: "",
+    deadline: ""
+  });
+
   const doneHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks[taskIndex].done = !tasks[taskIndex].done;
     setTaskState({tasks});
-  }
+  };
+
   const deleteHandler = (taskIndex) => {
     const tasks = [...taskState.tasks];
     tasks.splice(taskIndex, 1);
     setTaskState({tasks});
-  } 
+  };
+
+  const formChangeHandler = (event) => {
+    let form = {...formState};
+
+    switch(event.target.name) {
+      case "title":
+          form.title = event.target.value;
+          break;
+      case "description":
+          form.description = event.target.value;
+          break;
+      case "deadline":
+          form.deadline = event.target.value;
+          break;
+      default:
+          form = formState;
+    }
+    setFormState(form);
+  };
+
+  console.log(formState);
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const tasks = [...taskState.tasks];
+    const form = {...formState};
+
+    form.id = uuidv4();
+    
+    tasks.push(form);
+    setTaskState({tasks});
+  }
+    
+
   return (
     <div className="container">
       <h1>Tasky</h1>
@@ -29,14 +72,14 @@ function App() {
       title={task.title}
       description={task.description}
       deadline={task.deadline}
-      priority={task.priority}
+      priority={ task.priority}
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
       deleteTask = {() => deleteHandler(index)}
     />
   ))}
-  <AddTaskForm/>
+  <AddTaskForm submit={formSubmitHandler} change={formChangeHandler} />
     </div>
   );
 }
